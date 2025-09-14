df2b4a533e219c311a26606b4fdf8b6089bf6aa7
Henock-Lite <litehenock@gmail.com>
2025-09-14 02:17:31 +0100

    deprecate

--- assets/styles/js/index.js
+++ assets/styles/js/index.js
@@ -17,39 +17,9 @@ function tab() {
       listItem.classList.add("active");
       content[index].classList.add("activePage");
       
-      // const newUrl = listItem.getAttribute('data-nav') || `#${index}`;
-      // history.pushState({ tabIndex: index }, '', newUrl);
     });
   });
   
-  // Gérer le bouton précédent/navigation arrière
-  window.addEventListener('popstate', (e) => {
-    if (e.state && e.state.tabIndex !== undefined) {
-      const index = e.state.tabIndex;
-      
-      list.forEach((L) => {
-        L.classList.remove("activePage");
-        L.classList.remove("active");
-      });
-      content.forEach((C) => {
-        C.classList.remove("activePage");
-      });
-      
-      list[index].classList.add("active");
-      content[index].classList.add("activePage");
-    }
-  });
 }
 
-// window.addEventListener("DOMContentLoaded", () => {
-//   const loader = document.getElementById("loader");
-//   const content = document.getElementById("main-content");
-//   setTimeout(() => {
-//     loader.style.opacity = "0";
-//     setTimeout(() => {
-//       loader.style.display = "none";
-//       content.style.display = "block";
-//     }, 500);
-//   }, 3000);
-// });
 tab();
